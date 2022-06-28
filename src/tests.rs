b8f6ab9585edaa4e1f4d49aafc6974a228087b9d
Antti Keränen <detegr@rbx.email>
2022-06-28 18:51:49 +0300

    add tests for @

--- src/tests.rs
+++ src/tests.rs
@@ -312,3 +312,61 @@ fn should_panic_number_to_bqnvalue_vec() {
     })
     .is_err());
 }
+
+#[test]
+#[should_panic]
+fn should_panic_null_to_f64() {
+    let _ = BQNValue::null().into_f64();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_get_field() {
+    let _ = BQNValue::null().get_field("test");
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_has_field() {
+    let _ = BQNValue::null().has_field("test");
+}
+
+#[test]
+fn null_to_char() {
+    let _ = BQNValue::null().into_char();
+}
+
+#[test]
+fn null_to_u32() {
+    let _ = BQNValue::null().into_u32();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_to_f64_vec() {
+    let _ = BQNValue::null().into_f64_vec();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_to_i32_vec() {
+    let _ = BQNValue::null().into_i32_vec();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_to_bqnvalue_vec() {
+    let _ = BQNValue::null().into_bqnvalue_vec();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_to_string() {
+    let _ = BQNValue::null().into_string();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_null_to_char_vec() {
+    let _ = BQNValue::null().into_char_vec();
+}

0142d85c1048c5a8e4beb953adc54ce96e178640
Antti Keränen <detegr@rbx.email>
2022-06-30 16:26:31 +0300

    update documentation

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["cbqn","cbqn_sys","cfg_if","libc","lock_api","once_cell","parking_lot","parking_lot_core","scopeguard","smallvec"];
+window.ALL_CRATES = ["cbqn","cbqn_sys","cfg_if","getrandom","libc","lock_api","once_cell","parking_lot","parking_lot_core","ppv_lite86","rand","rand_chacha","rand_core","scopeguard","smallvec"];

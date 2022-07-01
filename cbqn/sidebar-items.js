89d2ce6f43ed49021ed5429a7eb776ef48e08b6c
Antti Keränen <detegr@rbx.email>
2022-07-01 09:21:07 +0300

    update documentation

--- cbqn/sidebar-items.js
+++ cbqn/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["eval","Evaluates BQN code"]],"macro":[["BQN","Convenience macro for running BQN expressions"]],"struct":[["BQNValue","Represents a BQN value"]]});
+initSidebarItems({"enum":[["BQNType","BQN type enumeration"]],"fn":[["eval","Evaluates BQN code"]],"macro":[["BQN","Convenience macro for running BQN expressions"]],"struct":[["BQNValue","Represents a BQN value"]]});

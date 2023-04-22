69c061677d3de04c2c60d5f25eb32e85408d8935
Antti Keränen <detegr@rbx.email>
2023-04-22 09:19:25 +0300

    move license

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,7 +1,3 @@
-// Copyright (C) 2022 Antti Keränen
-//
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
-
 use anyhow::Result;
 use cbqn_sys as bindings;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright (C) 2022-2023 Antti Keränen
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
 /*
 //! cbqn
 //!
